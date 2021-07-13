40876644ca2e90fa24bfe089ae517bf281874277
Mike <pigret-cell>
2021-07-13 11:00:51 +0900

    2

--- src/main/java/dev/mvc/team5/DatabaseConfiguration.java
+++ src/main/java/dev/mvc/team5/DatabaseConfiguration.java
@@ -18,7 +18,7 @@ import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @PropertySource("classpath:/application.properties")  // 설정 파일 위치
-@MapperScan(basePackages= {"dev.mvc.resort_v1sbm3a"})
+@MapperScan(basePackages= {"dev.mvc.team5_v2sbm3c_git"})
 public class DatabaseConfiguration {
     
     @Autowired
